fd98a8f8ce6a2a11d7f2a9af5ae9e3594146fea9
chxchenhaixiao <1852537158@qq.com>
2024-07-16 21:39:33 +0800

    tls

---
+++ ConcurrentMemoryPool/ConcurrentMemoryPool/Test.cc
@@ -0,0 +1,26 @@
+#define _CRT_SECURE_NO_WARNING 1
+#include <iostream>
+#include <thread>
+#include "ThreadCache.hpp"
+using std::cout;
+using std::endl;
+
+
+void CreateThreadCache()
+{
+	if (TLSthreadcache == nullptr)
+	{
+		TLSthreadcache = new ThreadCache;
+	}
+	for (int i = 0; i < 5; ++i) cout << std::this_thread::get_id()<<" : "<<TLSthreadcache << endl;
+}
+
+
+int main()
+{
+	std::thread t1(CreateThreadCache);
+	t1.join();
+	std::thread t2(CreateThreadCache);
+	t2.join();
+	return 0;
+}
