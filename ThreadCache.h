c23b77dbe57dc8541d9758361842cd70f0c9aaa8
Chen <1852537158@qq.com>
2025-03-13 16:27:59 +0800

    Update ThreadCache.h

--- ThreadCache.h
+++ ThreadCache.h
@@ -1,19 +1,16 @@
 ﻿#pragma once
 #include "PageCache.h"
 #include "CentralCache.h"
-
-class ThreadCache
-{
+class ThreadCache{
 private:
-	FreeList _freelists[NUM_LIST];	//每一个线程都拥有一组自由链表
-	
+	FreeList _freelists[NUM_LIST];	
 public:
 	ThreadCache() {}
-	void* Allocate(size_t bytes); //申请空间
-	void Deallocate(void* p);	//释放空间
+	void* Allocate(size_t bytes); 
+	void Deallocate(void* p);	
 private:
-	void* SolveListTooLong(FreeList& freelist, size_t n);	//指定某自由链表归还n块空间
-	void* FetchFromCentralCache(size_t index, size_t bytes); //bytes为经过对齐后的字节数
+	void* SolveListTooLong(FreeList& freelist, size_t n);	 
+	void* FetchFromCentralCache(size_t index, size_t bytes); 
 };
 
-static __declspec(thread) ThreadCache* TLSthreadcache = nullptr; //声明每一个线程独占一个threadcache
+static __declspec(thread) ThreadCache* TLSthreadcache = nullptr; 
